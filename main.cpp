85b57c2abe6383bc73d4ccfc23a2d3de0220f543
rcoucke <ruben.coucke@student.uhasselt.be>
2017-10-24 15:45:44 +0200

    Mooi venster gemaakt

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
 {
    QApplication app(argc, argv);
    MyWidget window;
-   window.resize(1000, 1000);
-   window.setWindowTitle(QApplication::translate("childwidget", "Boot"));
+   window.resize(900, 500);
+   window.setWindowTitle(QApplication::translate("childwidget", "Boot met metingen"));
    window.show();
 
    
